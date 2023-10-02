0b823b2187c1e3e88d89cea3a6136a160bbe1435
Gagan Dahiya <gagan3809@gmail.com>
2023-10-02 19:25:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.use('/', require('./routes'));
+
+app.listen(port,function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log(`Server running at port: ${port}`);
+});
